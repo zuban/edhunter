3dddf2d2b468e755f3cc6ef3930b3b9114f056c2
Sergey Zubov <serzbv@gmail.com>
2017-05-23 16:01:47 +0300

    #change frontend logic

--- src/main/java/com/heroku/demo/Application.java
+++ src/main/java/com/heroku/demo/Application.java
@@ -17,12 +17,18 @@ package com.heroku.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
 
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
 }
