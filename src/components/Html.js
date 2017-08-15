cd2c8e488d6cf10b515365c48bb4d8a477170a41
Sergey Zubov <serzbv@gmail.com>
2017-08-15 16:54:03 +0300

    #current work

--- src/components/Html.js
+++ src/components/Html.js
@@ -78,40 +78,24 @@ class Html extends React.Component {
             'ga(\'create\', \'UA-104645744-1\', \'auto\');' +
             'ga(\'send\', \'pageview\');'
           }}
-        />}
+        />
+      }
       {
         <script
           dangerouslySetInnerHTML={{
-            __html: '(function (d, w, c) {' +
-            '(w[c] = w[c] || []).push(function() {' +
-            'try {' +
-            'w.yaCounter45269349 = new Ya.Metrika({' +
-            'id:45269349,' +
-            'clickmap:true,' +
-            'trackLinks:true,' +
-            'accurateTrackBounce:true,' +
-            'webvisor:true,' +
-            'trackHash:true' +
-            '});' +
-            '} catch(e) { }' +
-            '});' +
-            'var n = d.getElementsByTagName("script")[0],' +
-            's = d.createElement("script"),' +
-            'f = function () { n.parentNode.insertBefore(s, n); };' +
-            's.type = "text/javascript";' +
-            's.async = true;' +
-            's.src = "https://mc.yandex.ru/metrika/watch.js";' +
-            'if (w.opera == "[object Opera]") {' +
-            'd.addEventListener("DOMContentLoaded", f, false);' +
-            '} else { f(); }' +
-            '})(document, window, "yandex_metrika_callbacks");'
+            __html: `try {` +
+            `var yaCounter45269349 = new Ya.Metrika({` +
+            `id:45269349,` +
+            `clickmap:true,` +
+            `trackLinks:true,` +
+            `accurateTrackBounce:true,` +
+            `webvisor:true` +
+            `});` +
+            `} catch(e) { }`
           }}
-        />}
-      {
-        <noscript>
-          <div><img src="https://mc.yandex.ru/watch/45269349" style="position:absolute; left:-9999px;" alt=""/></div>
-        </noscript>
+        />
       }
+      <script src="https://mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
       </body>
       </html>
     );
