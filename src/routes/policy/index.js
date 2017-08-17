3594352162f8e663340febaf3456abb481a25109
Sergey Zubov <serzbv@gmail.com>
2017-08-18 01:15:50 +0300

    current work

---
+++ src/routes/policy/index.js
@@ -0,0 +1,26 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React from 'react';
+import Layout from '../../components/Layout';
+import Education from './Education'
+
+function action() {
+  return {
+    chunks: ['education'],
+    title: 'Концепция перевернутого обучения',
+    component: (
+      <Layout>
+        <Education />
+      </Layout>
+    ),
+  };
+}
+
+export default action;
