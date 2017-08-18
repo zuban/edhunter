16973b460b089e1b56992de6afea7eaff53d4380
Sergey Zubov <serzbv@gmail.com>
2017-08-18 13:33:34 +0300

    #current work

--- src/components/Html.js
+++ src/components/Html.js
@@ -12,6 +12,22 @@ import PropTypes from 'prop-types';
 import serialize from 'serialize-javascript';
 import config from '../config';
 
+import appleIcon57 from '../favicon/apple-icon-57x57.png';
+import appleIcon60 from '../favicon/apple-icon-60x60.png';
+import appleIcon72 from '../favicon/apple-icon-72x72.png';
+import appleIcon76 from '../favicon/apple-icon-76x76.png';
+import appleIcon114 from '../favicon/apple-icon-114x114.png';
+import appleIcon120 from '../favicon/apple-icon-120x120.png';
+import appleIcon144 from '../favicon/apple-icon-144x144.png';
+import appleIcon152 from '../favicon/apple-icon-152x152.png';
+import appleIcon180 from '../favicon/apple-icon-180x180.png';
+import androidIcon192 from '../favicon/android-icon-192x192.png';
+import favicon32 from '../favicon/favicon-32x32.png';
+import favicon96 from '../favicon/favicon-96x96.png';
+import favicon16 from '../favicon/favicon-16x16.png';
+import manifest from '../favicon/manifest.json';
+import msIcon144 from '../favicon/ms-icon-144x144.png';
+
 /* eslint-disable react/no-danger */
 
 class Html extends React.Component {
@@ -46,6 +62,28 @@ class Html extends React.Component {
           </title>
           <meta name="description" content={description} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="apple-touch-icon" sizes="57x57" href={appleIcon57} />
+          <link rel="apple-touch-icon" sizes="60x60" href={appleIcon60} />
+          <link rel="apple-touch-icon" sizes="72x72" href={appleIcon72} />
+          <link rel="apple-touch-icon" sizes="76x76" href={appleIcon76} />
+          <link rel="apple-touch-icon" sizes="114x114" href={appleIcon114} />
+          <link rel="apple-touch-icon" sizes="120x120" href={appleIcon120} />
+          <link rel="apple-touch-icon" sizes="144x144" href={appleIcon144} />
+          <link rel="apple-touch-icon" sizes="152x152" href={appleIcon152} />
+          <link rel="apple-touch-icon" sizes="180x180" href={appleIcon180} />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href={androidIcon192}
+          />
+          <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+          <link rel="icon" type="image/png" sizes="96x96" href={favicon96} />
+          <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+          <link rel="manifest" href={manifest} />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content={msIcon144} />
+          <meta name="theme-color" content="#ffffff" />
           {scripts.map(script =>
             <link key={script} rel="preload" href={script} as="script" />,
           )}
@@ -83,7 +121,8 @@ class Html extends React.Component {
               }}
             />
           }
-          {<script
+          {
+            <script
               dangerouslySetInnerHTML={{
                 __html:
                   "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" +
@@ -93,8 +132,10 @@ class Html extends React.Component {
                   "ga('create', 'UA-104645744-1', 'auto');" +
                   "ga('send', 'pageview');",
               }}
-            />}
-          {<script
+            />
+          }
+          {
+            <script
               dangerouslySetInnerHTML={{
                 __html:
                   `try {` +
@@ -107,7 +148,8 @@ class Html extends React.Component {
                   `});` +
                   `} catch(e) { }`,
               }}
-            />}
+            />
+          }
           <script
             src="https://mc.yandex.ru/metrika/watch.js"
             type="text/javascript"
